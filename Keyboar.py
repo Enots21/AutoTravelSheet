2f6997a7ac4e511a13452f54b62e2754c2d69afc
Енот <68638093+Enots21@users.noreply.github.com>
2025-01-07 13:53:32 +0300

    Add files via upload

--- Keyboar.py
+++ Keyboar.py
@@ -1,4 +1,5 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 start_vod = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Зарегестрироваться', callback_data='start')]])
@@ -6,5 +7,20 @@ start_vod = InlineKeyboardMarkup(inline_keyboard=[
 cars_number = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Добавить авто', callback_data='numbercars')]])
 
+access_vod = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Записать данные', callback_data='yesaccess')]
+])
+
 info_vod = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Верно', callback_data='start_vod')]])
+    [InlineKeyboardButton(text='Верно', callback_data='sinfo')]])
+
+reinfoveh = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Перезаписать', callback_data='reinfove')],
+    [InlineKeyboardButton(text='Верно', callback_data='yesinfo')]
+])
+
+rep_start = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[
+    [KeyboardButton(text='Добавить новый путевой лист📄')],
+    [KeyboardButton(text='Выбрать путевой лист📇')],
+    [KeyboardButton(text='Информация о машине🚘')]
+])
---
+++ readme.md
@@ -0,0 +1,11 @@
+id INTEGER PRIMARY KEY,
+date Дата
+start_km Начальный Километр
+end_km Конец Километр
+total_km Общее Километр
+fuel_start Нач топливо
+fuel_end Конец топливо
+fuel_refuel сколько заправился
+user_name Имя и Фамилия
+special_equipment Спец оборудование
+nuber_car Номер машины
